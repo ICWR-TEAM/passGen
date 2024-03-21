5cbe346e2ba28ec527d703702cff8a376fc6f6d6
Jon3sjns <Jon3sjns@github.com>
2024-03-21 09:30:18 +0700

    updated: website version and cli version

--- main.php
+++ main.php
@@ -9,6 +9,7 @@ class Main
 	private $arr;
 	function __construct()
 	{
+
 		// variable
 		$this->lower = "abcdefghijklmnopqrstuvwxyz";
 		$this->upper = strtoupper($this->lower);
@@ -17,28 +18,65 @@ class Main
 		$this->join = $this->lower.$this->upper.$this->num.$this->symb;
 		$this->arr = str_split($this->join);
 
-		// exec
-		echo "*********************************\n";
-		echo "        Password Generator       \n";
-		echo "*********************************\n";
-		echo "How many characters: ";
-		$count_char = (int) trim(fgets(STDIN));
-		$this->proc($count_char);
+		if( php_sapi_name() == "cli" ){
+
+			// parse
+			$options = getopt("c:h", ["char::"]);
+			$count_char = @$options["c"];
+			$chare = @$options["char"] ? str_split( @$options["char"] ) : $this->arr;
+
+			if( isset($options["h"])){
+				echo "php main.php -n 'count-character' --char='character'\n";
+				echo "   -n 'count-character' Default 12 Character\n";
+				echo "   --char='character' Default 0-9a-Z and Symbols\n";
+				exit();
+			}
+
+			if( isset($count_char) || !$count_char){
+				echo "*********************************\n";
+				echo "        Password Generator       \n";
+				echo "*********************************\n";
+                $this->proc_terminal($count_char ? $count_char : 12, $chare);
+				exit();
+			}
+
+		}
+
 	}
-	public function proc($count_char)
+	public function proc_terminal($count_char, $chare)
 	{
 		if ($count_char) {
 			$jumlah_char = $count_char;
-			$char = "";
-			for($i=0; $i<=$jumlah_char-1; $i++){
-				$rand = rand(0,count($this->arr)-1);
-				$char .= $this->arr[$rand];
+			$res_char = "";
+			for($i=0; $i<=(int)$jumlah_char-1; $i++){
+				$rand = rand(0,count($chare)-1);
+				$res_char .= $chare[$rand];
 			}
-			echo "Result: ".$char."\n";
+			echo "Result: ".$res_char."\n";
 		}else {
 			echo "Use numbers!\n";
 		}
 	}
+
+	function proc_website($char = false, $count = 12)
+	{
+		if(!is_numeric($count)){
+			return "The number of characters must be a number";
+			exit();
+		}
+
+		$character = $char ? str_split($char) : $this->arr;
+		if($character && $count){
+			$res_char = "";
+			for ($i=0; $i < (int)$count; $i++) { 
+				$rand = rand(0, count($character)-1);
+				$res_char .= $character[$rand];
+			}
+			return $res_char;
+		}
+	}
 }
-new Main();
+$asd = new Main();
+echo  $asd->proc_website(char:"aA", count:"12");
+
 ?>
